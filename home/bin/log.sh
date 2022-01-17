33b5ce92982c72239c5a30afd512d15615f25186
Infinene <am@infinene.com>
2022-01-16 21:44:51 -0600

    fix function definitions

--- home/bin/log.sh
+++ home/bin/log.sh
@@ -1,11 +1,11 @@
 KEEP_LINES=4000
-function create_log() {
+create_log() {
     if [ -f $1 ]; then
         LOG=$(tail -n $KEEP_LINES $1 2>/dev/null) && echo "${LOG}" >$1
     fi
     exec > >(tee -a $1)
     exec 2>&1
 }
-function log() {
+log() {
     echo "[$(date --rfc-3339=seconds)] $*"
 }
