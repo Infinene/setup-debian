6bb4d3577a23c6334ae4fc34eccd7fccab4cf87a
Infinene <infinene@gmail.com>
2023-06-19 23:42:02 -0500

    refactor

--- home/.local/bin/src/update_micro.sh
+++ home/.local/bin/src/update_micro.sh
@@ -2,7 +2,6 @@ printf '%s\n'
 printf '%s\n' "Updating micro editor..."
 
 repo="zyedidia/micro"
-bin_dir="/usr/local/bin"
 
 setup_alternatives () {
   if ! update-alternatives --list editor | grep "micro" >/dev/null; then
@@ -14,8 +13,13 @@ setup_alternatives () {
 
 if command -v micro &> /dev/null; then
   cur_version="$(micro -version | grep -Eo "[0-9]+\.[0-9]+(\.[0-9]+)?")"
-elif [ ! -f /usr/local/bin/micro ] then
+elif [ -f ~/.local/bin/micro ]; then
   cur_version="$(~/.local/bin/micro -version | grep -Eo "[0-9]+\.[0-9]+(\.[0-9]+)?")"
+  if [ ! -f /usr/local/bin/micro ]; then
+      printf "creating link in /usr/local/bin ...\n"
+      sudo ln -s $HOME/.local/bin/micro /usr/local/bin/
+      setup_alternatives
+  fi
 else
   cur_version="0.0"
 fi
@@ -24,12 +28,7 @@ new_version="$(get_latest_release_num $repo)"
 file=micro-"${new_version}-${arch}.deb"
 
 if [ "${cur_version}" \> "${new_version}" ] || [ "${cur_version}" = "${new_version}" ]; then
-    printf '%s\n' "Already at latest version: micro ${cur_version}"
-    if [ ! -f /usr/local/bin/micro ]; then
-	      printf "micro setup ...\n"
-	      sudo ln -s $HOME/.local/bin/micro /usr/local/bin/
-        setup_alternatives
-    fi
+    printf "Already at latest version: micro ${cur_version}\n"
 else
     # get the package
     wget -nv --show-progress "https://github.com/${repo}/releases/download/v${new_version}/${file}"
@@ -38,5 +37,8 @@ else
     # remove the file
     rm -vf $file
     setup_alternatives
+    printf "remove local version from ~/.local/bin if installed ..."
+    printf "Press enter to continue "
+    read key
 fi
 
--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # set -x
 
-printf '%s\n'
-printf '%s\n' '               Debian Web Server Setup                     '
-printf '%s\n' '               -----------------------                     '
-printf '%s\n' '  This will setup basic shell environment and install php  '
-printf '%s\n' '  MariaDB and nginx to create a web server.                '
+echo
+echo '               Debian Web Server Setup                     '
+echo '               -----------------------                     '
+echo '  This will setup basic shell environment and install php  '
+echo '  MariaDB and nginx to create a web server.                '
 
 userid=$(id -u)
 mariadb_ver='10.11'
@@ -13,23 +13,24 @@ php_ver='8.2'
 
 if [[ $userid -eq 0 ]]; then
     tput setaf 1;
-    printf '%s\n' "\nRunning as root! User environment and utilities will not be setup."
+    printf "\nRunning as root! User environment and utilities will not be setup.\n"
     tput init
-    printf '%s\n' "Launch as a user in sudo group for complete setup."
+    printf "Launch as a user in sudo group for complete setup.\n"
     while true
     do
-        read -p "Continue as root user (y/n)? " choice
+        printf "Continue as root user (y/n)? "
+        read choice
         case "$choice" in
         y|Y )   break;;
         n|N )   exit
                 break;;
-        * )     printf '%s\n' "please enter y/Y or n/N"
+        * )     echo "please enter y/Y or n/N"
                 ;;
         esac
     done
 else
     SUDO=sudo
-    ${SUDO} printf '%s\n' 
+    ${SUDO} echo 
 fi
 
 is_wsl=$(grep -i microsoft /proc/version)
@@ -38,9 +39,9 @@ setup_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 . $setup_dir/misc/multiselect
 . $setup_dir/misc/read_secret
 
-printf '%s\n'
-printf '%s\n' "Select / unselect options to install:"
-printf '%s\n' '-------------------------------------'
+echo
+echo "Select / unselect options to install:"
+echo '-------------------------------------'
 
 
 menu=("Shell setup" "MariaDB ${mariadb_ver}" "NginX (mainline)" "PHP ${php_ver}" )
@@ -62,21 +63,24 @@ fi
 if [ ${selections[1]} = "true" ]; then
     echo
     echo "Installing and configuring MariaDB ${mariadb_ver}"
-    read -p "Press enter to continue"
+    printf "Press enter to continue"
+    ready key
     . $setup_dir/setup.d/_mariadb.sh
 fi
 
 if [ ${selections[2]} = "true" ]; then
     echo
     echo "Installing and configuring latest NginX"
-    read -p "Press enter to continue"
+    printf "Press enter to continue"
+    read key
     . $setup_dir/setup.d/_nginx.sh
 fi
 
 if [ ${selections[3]} = "true" ]; then
     echo
     echo "Installing and configuring PHP ${php_ver}"
-    read -p "Press enter to continue"
+    printf "Press enter to continue"
+    read key
     . $setup_dir/setup.d/_php.sh
 fi
 
@@ -86,19 +90,14 @@ echo "Restarting services ..."
 [ ${selections[2]} = true ] && ${SUDO} systemctl restart nginx
 [ ${selections[3]} = true ] && ${SUDO} systemctl restart php${php_ver}-fpm
 
-printf '%s\n'
-printf '%s\n' 'Setup completed!'
-printf '%s\n' '----------------'
-printf '%s\n' '. ~/.profile to reload bash config.'
-printf '%s\n' 'run setup_webutils to install website maintenance utils.'
-printf '%s\n' 'run setup_mailhog to install a local mail catcher'
-printf '%s\n' 'run setup_samba to install samba'
-printf '%s\n' 'run setup_lego to install letsencrypt client'
-# if [ ${selections[1]} = "true" ]; then
-#     printf '%s\n'
-#     printf '%s\n' 'Consider adding mysql superuser for web based administration:'
-#     printf '%s\n' "sudo mysql -e \"CREATE USER ${USER}@'%' IDENTIFIED BY 'passwd'; \\" 
-#     printf '%s\n' "GRANT ALL PRIVILEGES ON *.* TO ${USER}@'%' WITH GRANT OPTION;\""
-# fi
-printf '%s\n' '----------------'
-printf '%s\n'
+echo
+echo 'Setup completed!'
+echo '----------------'
+echo '. ~/.profile to reload bash config.'
+echo 'run setup_webutils to install website maintenance utils.'
+echo 'run setup_mailhog to install a local mail catcher'
+echo 'run setup_samba to install samba'
+echo 'run setup_lego to install letsencrypt client'
+
+echo '----------------'
+echo
