572669c91c006b02e5edef07653b655cebdc9e53
infinene <infinene@gmail.com>
2024-10-02 19:57:08 -0500

    Fix eget

--- home/.local/bin/bin.d/_get-eget.sh
+++ home/.local/bin/bin.d/_get-eget.sh
@@ -6,11 +6,13 @@ repo="zyedidia/eget"
 new_version=$(github_latest_release_num $repo)
 if command -v eget &> /dev/null; then
   cur_version="$(eget -v | grep -Eo "[0-9]+\.[0-9]+(\.[0-9]+)?")"
+  if [ "$cur_version" = "${new_version}" ]; then
+    printf '%s\n' "Already at latest version: eget $cur_version"
+  else
+    ${SUDO} eget --to=/usr/local/bin $repo
+  fi
 else
   cur_version="0.0"
-fi
-if [ "$cur_version" = "${new_version}" ]; then
-    printf '%s\n' "Already at latest version: eget $cur_version"
-else
-  ${SUDO} eget --to=/usr/local/bin $repo
+  curl https://zyedidia.github.io/eget.sh | sh
+  ${sudo} mv eget /usr/local/bin/
 fi
