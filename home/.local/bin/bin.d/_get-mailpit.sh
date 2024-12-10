5ea4bfe53922cc0764d615180769e3beb5dc16c3
infinene <infinene@gmail.com>
2024-12-10 01:03:11 -0600

    Update mailpit installation

--- home/.local/bin/bin.d/_get-mailpit.sh
+++ home/.local/bin/bin.d/_get-mailpit.sh
@@ -3,18 +3,30 @@ printf '%s\n' "Updating mailpit..."
 
 repo="axllent/mailpit"
 
+${SUDO} eget --to=/usr/local/bin --upgrade-only $repo
 
-# new_version=$(github_latest_release $repo)
-# if command -v mailpit &> /dev/null; then
-#   cur_version="$(mailpit version | grep -Eo "v[0-9]+\.[0-9]+(\.[0-9]+)?")"
-# else
-#   cur_version="0.0"
-# fi
-# if [ "${cur_version}" = "${new_version}" ]; then
-#     printf '%s\n' "Already at latest version: mailpit $cur_version"
-# else
-  ${SUDO} eget --to=/usr/local/bin --upgrade-only $repo
-# fi
+if [ ! -f /etc/systemd/system/mailpit.service ]; then
+
+  ${SUDO} tee /etc/systemd/system/mailpit.service <<_END_
+[Unit]
+Description=Mailpit server
+
+[Service]
+ExecStart=/usr/local/bin/mailpit -d /var/lib/mailpit/mailpit.db
+Restart=always
+# Restart service after 10 seconds service crashes
+RestartSec=10
+SyslogIdentifier=mailpit
+User=mailpit
+Group=mailpit
+
+[Install]
+WantedBy=multi-user.target
+_END_
+
+  ${SUDO} systemctl enable mailpit.service
+  ${SUDO} systemctl enable mailpit.service
+fi
 
 printf "Press enter to continue "
 read key
