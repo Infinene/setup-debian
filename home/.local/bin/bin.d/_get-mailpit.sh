95fea7f652159d565df15b447de87f11c2c828e8
infinene <infinene@gmail.com>
2024-12-11 23:53:09 -0600

    Fix mailpit installation

--- home/.local/bin/bin.d/_get-mailpit.sh
+++ home/.local/bin/bin.d/_get-mailpit.sh
@@ -17,16 +17,28 @@ Restart=always
 # Restart service after 10 seconds service crashes
 RestartSec=10
 SyslogIdentifier=mailpit
-User=mailpit
-Group=mailpit
+User=mail
+Group=mail
 
 [Install]
 WantedBy=multi-user.target
 _END_
-
-  ${SUDO} systemctl enable mailpit.service
-  ${SUDO} systemctl enable mailpit.service
 fi
+
+${SUDO} mkdir -p /var/lib/mailpit
+${SUDO} chown mail:mail /var/lib/mailpit
+
+${SUDO} systemctl enable mailpit.service
+${SUDO} systemctl start mailpit.service
+
+# Test mail
+cat <<EOF | /usr/local/bin/mailpit sendmail -t
+To: recipient@example.com
+Subject: Testing
+From: sender@example.com
+
+This is a test message
+EOF
 
 printf "Press enter to continue "
 read key
