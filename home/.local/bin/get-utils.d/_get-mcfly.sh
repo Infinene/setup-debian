2771614eaef2de7261f4288ba31e9683cdd8bb6c
infinene <infinene@gmail.com>
2024-12-14 17:39:59 -0600

    Add nmon and mcfly

---
+++ home/.local/bin/get-utils.d/_get-mcfly.sh
@@ -0,0 +1,11 @@
+printf '%s\n'
+printf '%s\n' "Updating mcfly..."
+set -x
+
+repo="cantino/mcfly"
+
+${SUDO} eget -a x86_64 -a linux --upgrade-only --to=/usr/local/bin $repo
+grep -q -e 'mcfly' ~/.bashrc || echo 'eval "$(mcfly init bash)"' | tee -a ~/.bashrc
+
+printf "Press enter to continue "
+read key
