61d00c19c01f7b3f4ae5902a7c71f4f1f115cf26
infinene <infinene@gmail.com>
2024-12-14 12:33:20 -0600

    Add peco to utils

---
+++ home/.local/bin/get-utils.d/_get-peco.sh
@@ -0,0 +1,10 @@
+printf '%s\n'
+printf '%s\n' "Updating peco..."
+set -x
+
+repo="peco/peco"
+
+${SUDO} eget -a linux_amd64 --upgrade-only --to=/usr/local/bin $repo
+
+printf "Press enter to continue "
+read key
