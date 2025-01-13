95432b97e8ac770fd493aea87c2d39cb915ebc01
Infinene <infinene@gmail.com>
2025-01-13 16:45:49 -0600

    Convert to tabs

--- src/http/utils/mail-test.php
+++ src/http/utils/mail-test.php
@@ -52,77 +52,77 @@ if(isset($_POST["mail"], $_POST["subject"], $_POST["email"], $_POST["myemail"]))
 ?>
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="UTF-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1" />
-		<title>PHP Email Test</title>
-		<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css"
-			type="text/css" rel="stylesheet" />
-	</head>
-	<body>
-		<div class="container">
-			<h2>Send an email from localhost</h2>
-			<form onsubmit="sendmail(event)" id="myForm" action="#" method="post">
-				<div class="form-group">
-					<label for="myemail">Email From</label>
-					<input type="email" class="form-control" value="php@localhost"
-						name="myemail" placeholder="Email sender" id="myemail" required />
-				</div>
-				<div class="form-group">
-					<label for="email">Email to</label>
-					<input type="email" class="form-control" value="admin@example.com"
-						name="email" placeholder="Email recipient" id="email" required />
-				</div>
-				<div class="form-group">
-					<label for="subject">Subject</label>
-					<input type="text" class="form-control" value="php test email"
-						name="subject" placeholder="Subject" id="subject" required />
-				</div>
-				<div class="form-group" id="help-parent">
-					<label for="mail">Email Body</label>
-					<textarea name="mail" class="form-control" id="mail" rows="5" required>
-						<h2>Sent from localhost!</h2>
-					</textarea>
-					<span id="response" class="help-block"></span>
-				</div>
-				<button type="submit" class="btn btn-info">Send Mail</button>
-			</form>
-		</div>
-		<script>
-		function sendmail(event){
-			event.preventDefault();
-			var mail = document.getElementById("mail").value;
-			var email = document.getElementById("email").value;
-			var myemail = document.getElementById("myemail").value;
-			var subject = document.getElementById("subject").value;
-			const toSend = {
-				method : 'post',
-				headers: {
-					"Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
-				},
-				body : "email="+encodeURIComponent(email)+
-						"&subject="+encodeURIComponent(subject)+
-						"&mail="+encodeURIComponent(mail)+
-						"&myemail="+encodeURIComponent(myemail)
-			};
-			fetch('<?=$_SERVER["REQUEST_URI"]?>', toSend)
-			.then(function(response) {
-				return response.json();
-			})
-			.then(function(data) {
-				document.getElementById("response").innerHTML = data.text;
-				if(data.success){
-					document.getElementById("help-parent").classList.add("has-success");
-					document.getElementById("help-parent").classList.remove("has-error");
-				}else{
-					document.getElementById("help-parent").classList.remove("has-success");
-					document.getElementById("help-parent").classList.add("has-error");
-				}
-			}).catch(function(err){
-				alert("The request resulted in an error.");
-				console.warn(err);
-			});
-		}
-		</script>
-	</body>
+<head>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<title>PHP Email Test</title>
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css"
+		type="text/css" rel="stylesheet" />
+</head>
+<body>
+	<div class="container">
+		<h2>Send an email from localhost</h2>
+		<form onsubmit="sendmail(event)" id="myForm" action="#" method="post">
+			<div class="form-group">
+				<label for="myemail">Email From</label>
+				<input type="email" class="form-control" value="php@localhost"
+					name="myemail" placeholder="Email sender" id="myemail" required />
+			</div>
+			<div class="form-group">
+				<label for="email">Email to</label>
+				<input type="email" class="form-control" value="admin@example.com"
+					name="email" placeholder="Email recipient" id="email" required />
+			</div>
+			<div class="form-group">
+				<label for="subject">Subject</label>
+				<input type="text" class="form-control" value="php test email"
+					name="subject" placeholder="Subject" id="subject" required />
+			</div>
+			<div class="form-group" id="help-parent">
+				<label for="mail">Email Body</label>
+				<textarea name="mail" class="form-control" id="mail" rows="5" required>
+					<h2>Sent from localhost!</h2>
+				</textarea>
+				<span id="response" class="help-block"></span>
+			</div>
+			<button type="submit" class="btn btn-info">Send Mail</button>
+		</form>
+	</div>
+	<script>
+	function sendmail(event){
+		event.preventDefault();
+		var mail = document.getElementById("mail").value;
+		var email = document.getElementById("email").value;
+		var myemail = document.getElementById("myemail").value;
+		var subject = document.getElementById("subject").value;
+		const toSend = {
+			method : 'post',
+			headers: {
+				"Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
+			},
+			body : "email="+encodeURIComponent(email)+
+					"&subject="+encodeURIComponent(subject)+
+					"&mail="+encodeURIComponent(mail)+
+					"&myemail="+encodeURIComponent(myemail)
+		};
+		fetch('<?=$_SERVER["REQUEST_URI"]?>', toSend)
+		.then(function(response) {
+			return response.json();
+		})
+		.then(function(data) {
+			document.getElementById("response").innerHTML = data.text;
+			if(data.success){
+				document.getElementById("help-parent").classList.add("has-success");
+				document.getElementById("help-parent").classList.remove("has-error");
+			}else{
+				document.getElementById("help-parent").classList.remove("has-success");
+				document.getElementById("help-parent").classList.add("has-error");
+			}
+		}).catch(function(err){
+			alert("The request resulted in an error.");
+			console.warn(err);
+		});
+	}
+	</script>
+</body>
 </html>
