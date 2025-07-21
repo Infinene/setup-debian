ad594df15c6c844b1f1c8839086bffa63c09c972
Infinene <infinene@gmail.com>
2025-07-20 20:21:49 -0500

    Changes for trixie

--- src/localsite/utils/mail-test.php
+++ src/localsite/utils/mail-test.php
@@ -24,7 +24,7 @@ if (isset($_POST["mail"], $_POST["subject"], $_POST["email"], $_POST["myemail"])
 			<head>
 				<meta charset="UTF-8" />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<title>MailHog email example</title>
+				<title>Email example</title>
 			</head>
 			<body>
 				' . $_POST["mail"] . '
