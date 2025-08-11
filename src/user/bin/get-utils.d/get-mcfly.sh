d14bd7d3602ba72163bb1b49cba78261aa867756
Infinene <infinene@gmail.com>
2025-08-11 17:55:54 -0500

    Change prompt

--- src/user/bin/get-utils.d/get-mcfly.sh
+++ src/user/bin/get-utils.d/get-mcfly.sh
@@ -5,6 +5,5 @@ printf '%s\n' "Updating mcfly..."
 repo="cantino/mcfly"
 
 ${SUDO} eget -a x86_64 -a linux --upgrade-only --to=/usr/local/bin $repo
-grep -q -e 'mcfly' ~/.bashrc || echo 'eval "$(mcfly init bash)"' | tee -a ~/.bashrc
 
 read -rsn1 -p "Press any key to continue... ";echo
